50ed6a7392c220aafdaee72741f3e68bf02adfee
mario8a <mario8ato@hotmail.com>
2020-06-09 15:03:47 -0500

    Agregando boton para el checkOut

---
+++ src/components/CheckoutTemp.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default ({sku}) => {
+    return(
+        <div>
+            <button className="app-btn text-xl">Comprar</button>
+        </div>
+    )
+}
--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import Price from './Price';
+import CheckoutTemp from './CheckoutTemp';
 
 export default ({producto, skus}) => {
     const sku = skus[0];
@@ -13,13 +15,21 @@ export default ({producto, skus}) => {
                             : <img src={producto.images} alt=""/>
                         }
                     </div>
-                    <div className="flex-1">
-                        <p> {sku.unit_amount} {sku.currency} </p>
+                    <div className="flex-1 px-6">
+                        <p>Precio: </p>
+                        <Price
+                            price={sku.unit_amount}
+                            currency={sku.currency}
+                            className="text-2xl"
+                        />
                     </div>
                 </div>
             </header>
             <div className="text-center p-6">
                 <h1 className="text-3xl uppercase mb-4"> {producto.name} </h1>
+                <CheckoutTemp
+                    sku={sku}
+                />
             </div>
         </article>
     )
