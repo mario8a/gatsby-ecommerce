91406e97aaf6e94837b0d0adc55d38ab9cfb4e85
mario8a <mario8ato@hotmail.com>
2020-06-11 22:16:33 -0500

    mehh

--- src/components/Total.jsx
+++ src/components/Total.jsx
@@ -1,32 +1,16 @@
 import React from 'react';
-// import Price from './Price';
+import Price from './Price';
 
 export default ({shoppingCart}) => {
-    console.log("Desde total", shoppingCart);
+    //PROBLEMA TENGO UN OBJETO CON DOS OBJETOS DENTROS
 
-    // const aversi = shoppingCart.map(({sku}) => {
-    //     let cantidad = sku.unit_amount
-    //     console.log({cantidad});
-        
-    // });
-
-    // const total = aversi.reduce(
-    //     (total,cantidad) => parseInt(cantidad + total)
-    // , 0);
-
-    // console.log(total);
-    
-    // const total = shoppingCart.reduce(
-    //     (total, producto) => producto.unit_amount + total
-    // ,0)
-
-    // console.log(total);
-
-    // console.log("TOTAL", total);
+    const total = shoppingCart.reduce(
+        (total, product)=> (product.prunit_amountice * product.quantity) + total
+      , 0)
     return(
         <div>
             <p className="uppercase text-white">Total</p>
-            {/* <Price price={total} currency={"MXN"}  className="text-2xl"/> */}
+            <Price price={total} currency={"MXN"} className="text-2xl" />
         </div>
     )
 }
